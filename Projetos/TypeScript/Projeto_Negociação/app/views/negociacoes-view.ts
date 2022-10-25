0985dcc0e632bb1d96c839c079a62de42a3e21d4
Rafael Tomas Souza dos Santos <rafael.tomas@mv.com.br>
2022-10-25 07:55:51 -0300

    projeto(type): criando view

--- Projetos/TypeScript/Projeto_Negociação/app/views/negociacoes-view.ts
+++ Projetos/TypeScript/Projeto_Negociação/app/views/negociacoes-view.ts
@@ -1,12 +1,9 @@
-export class NegociacoesView {
+import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
+export class NegociacoesView extends View<Negociacoes> {
 
-  private elemento: HTMLElement;
-
-  constructor(seletor: string){
-    this.elemento = document.querySelector(seletor);
-  }
-    template(): string {
-      return `
+  template(model: Negociacoes): string {
+    return `
         <table class="table table-hover table-bored">
           <thead>
             <tr>
@@ -16,12 +13,22 @@ export class NegociacoesView {
             </tr>
           </thead>
           <tbody>
+            ${model.lista().map(negociacao => {
+      return `
+                  <tr>
+                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                    <td>${negociacao.quantidade}</td>
+                    <td>${negociacao.valor}</td>
+                  </tr>
+                `;
+    }).join('')}
           </tbody>
         </table>
       `;
-    }
+  }
 
-    update(): void{
-      this.elemento.innerHTML = this.template();
-    }
+  update(model: Negociacoes): void {
+    const template = this.template(model);
+    this.elemento.innerHTML = template;
+  }
 }
