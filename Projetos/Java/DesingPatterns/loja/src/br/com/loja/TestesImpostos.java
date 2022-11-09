5cdba7cb7fec2623c73f27f4de09f7a9be57797f
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-11-09 10:06:30 -0300

    Projeto(JAVA): Desing patterns chain of responsability

--- Projetos/Java/DesingPatterns/loja/src/br/com/loja/TestesImpostos.java
+++ Projetos/Java/DesingPatterns/loja/src/br/com/loja/TestesImpostos.java
@@ -2,20 +2,28 @@ package br.com.loja;
 
 import java.math.BigDecimal;
 
-import br.com.loja.imposto.CalculadoraDeImposto;
-import br.com.loja.imposto.ICMS;
-import br.com.loja.imposto.ISS;
-import br.com.loja.imposto.Imposto;
+import br.com.loja.desconto.CalculadoraDeDesconto;
+//import br.com.loja.imposto.CalculadoraDeImposto;
+//import br.com.loja.imposto.ICMS;
+//import br.com.loja.imposto.ISS;
+//import br.com.loja.imposto.Imposto;
 import br.com.loja.orcamento.Orcamento;
 
 public class TestesImpostos {
 
 	public static void main(String[] args) {
 
-		Orcamento orcamento = new Orcamento(new BigDecimal("3500"));
-		CalculadoraDeImposto calculadora = new CalculadoraDeImposto();
-		System.out.println(calculadora.calcular(orcamento, new ICMS()));
-
+		Orcamento primeiro = new Orcamento(new BigDecimal("200"),6);
+		Orcamento segundo = new Orcamento(new BigDecimal("1000"),1);
+		
+//		CalculadoraDeImposto calculadora = new CalculadoraDeImposto();
+//		System.out.println(calculadora.calcular(primeiro, new ICMS()));
+//		System.out.println(calculadora.calcular(segundo, new ISS()));
+		
+		CalculadoraDeDesconto calculadora = new CalculadoraDeDesconto();
+		System.out.println(calculadora.calcular(primeiro));
+		System.out.println(calculadora.calcular(segundo));
+		
 	}
 
 }
